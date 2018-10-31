02b2bac3278380c015270b818977c25fd94d1bf0
Anthony Chao <anthony.chao@blackrock.com>
2018-10-30 17:17:46 -0700

    Completed about_triangle_project2.py, see triangle.py

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,12 +17,23 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    if (a == b and b == c):
-        return 'equilateral'
-    elif (a == b or b == c or a == c):
-        return 'isosceles'
-    elif (a != b  and b != c and a != c):
-        return 'scalene'
+    sides = sorted([a, b, c])
+    
+    if sides[0] == 0:
+        raise TriangleError('All sides should be greater than 0')
+    elif sides[0] < 0:
+        raise TriangleError('All sides should be greater than 0')
+    elif sides[0] + sides[1] < sides[2]:
+        raise TriangleError('Sum of any two sides should be greater than the third')
+
+    types = { 
+        1: 'equilateral', 
+        2: 'isosceles', 
+        3: 'scalene'
+    }
+    
+    num_uniq_sides = len(set(sides))
+    return types[num_uniq_sides]
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
